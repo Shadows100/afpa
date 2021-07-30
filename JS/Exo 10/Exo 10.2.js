de87090995b44be58e23453a03f56a96f2f6bdb6
Shadows100 <87180297+Shadows100@users.noreply.github.com>
2021-07-30 11:33:30 +0200

    commit 17

--- JS/Exo 10/Exo 10.2.js
+++ JS/Exo 10/Exo 10.2.js
@@ -5,44 +5,45 @@ var sai;
 
 
 
-
+// Lire un entier au clavier 
 
 function getinteger () {
 
+         taille = parseInt(prompt("Entrez un nombre pour le nombre d'entrée pour le tableau"));
+            if (Number.isInteger( taille)) {
+                return taille;
 
-
-
-
-
+        }
 }
+taille = getinteger ()
+
+// créer et initialiser un tableau 
+
 
 function initTab () {
 
-
-
-
-
-
+    tableau = new array (taille);
+    return tableau;
 
 }
 
+
+// permet la saisie des différents élément du tableau
+
 function SaisieTab () {
 
+     for (var i = 0 ; i < taille ; i++ ); {
+           tableau[i]  = parseInt(prompt("Entrez un post : "));
 
-
-
-
-
-
+        }
 }
 
 function AfficheTab () {
 
-
-
-
-
-
+        for (var i = 0 ; i < taille ; i++ );{
+        num = i+ 1
+        document.write(" l'élement N° " + num + " correspond à : " + tableau[i] + "<br>")
+    }
 }
 
 function RechercheTab () {
@@ -58,7 +59,5 @@ function infoTab () {
 
 
 
-
-    
 
 }
