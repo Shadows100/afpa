5cc86d26afcc53a35ce148ef1d1ad2022fca5967
Shadows100 <87180297+Shadows100@users.noreply.github.com>
2021-07-30 10:50:15 +0200

    commit 16

---
+++ JS/Exo 10/Exo 10.2.js
@@ -0,0 +1,64 @@
+var maxi = 0;
+var moy = 0;
+var taille = 0;
+var sai;
+
+
+
+
+
+function getinteger () {
+
+
+
+
+
+
+}
+
+function initTab () {
+
+
+
+
+
+
+
+}
+
+function SaisieTab () {
+
+
+
+
+
+
+
+}
+
+function AfficheTab () {
+
+
+
+
+
+
+}
+
+function RechercheTab () {
+
+
+
+
+
+
+}
+
+function infoTab () {
+
+
+
+
+    
+
+}
