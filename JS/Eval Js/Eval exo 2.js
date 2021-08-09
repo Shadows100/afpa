94b2ed8f79d090c2c0145fdffb3323cd00357941
Shadows100 <87180297+Shadows100@users.noreply.github.com>
2021-08-09 16:40:43 +0200

    18 éme commit

---
+++ JS/Eval Js/Eval exo 2.js
@@ -0,0 +1,11 @@
+TableMultiplication(7);
+
+function TableMultiplication(muli) {
+var result = 0;
+
+for(var i = 1; i <= 10 ; i++) {
+    resultat = muli * i;
+    document.write(i + "x" + muli + " = " + resultat + '<br>')
+
+}
+}
---
+++ JS/Eval Js/Eval exo 3.js
@@ -0,0 +1,3 @@
+var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+var saisie = window.prompt("Veuiller saisir un prénom. \n *Commencez toujours par une majuscule*");
+
