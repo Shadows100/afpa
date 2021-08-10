351638e22df7d203e5acf24c4f454daf7990b6e5
Shadows100 <87180297+Shadows100@users.noreply.github.com>
2021-08-10 16:54:13 +0200

    23 éme commit

--- JS/Eval Js/Eval 5/Eval exo 5.js
+++ JS/Eval Js/Eval 5/Eval exo 5.js
@@ -5,13 +5,14 @@ function valider(event) {
 
     
 var Nom = fromCoordonees.elements["Nom"]
-var prenom = fromCoordonees.elements["Prénom"]
-var date = fromCoordonees.elements["date_de_naissance"]
-var code = fromCoordonees.elements["Code-Postal"]
-var ville = document.getElementById("Ville");
+var Prenom = fromCoordonees.elements["Prénom"]
+var Date = fromCoordonees.elements["date_de_naissance"]
+var Code = fromCoordonees.elements["Code-Postal"]
+var Ville = document.getElementById("Ville");
+var Adresse = document.getElementById("Adresse");
 var Email = fromCoordonees.elements["Email"]
-var sujet = fromCoordonees.elements["Sujet"]
-var question = fromCoordonees.elements["question"]
+var Sujet = fromCoordonees.elements["Sujet"]
+var Question = fromCoordonees.elements["question"]
 var erreur;
 
 var regex1 = /^[a-z0-9]?([_.-]?[a-z0-9])*@[a-z0-9]+([_.-]?[a-z0-9]+)?[.]{1}[a-z]{2,6}$/;
@@ -21,35 +22,105 @@ var regex5 = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-Zéèî�
 
 }
 
+// Nom
 
 if (Nom.value == "") {
     event.preventDefault()
-    erreur = "Sasie to nom fdp";
-    document.getElementById("erreur3").innerHTML= erreur;
+    erreur = "Sasie ton nom fdp";
+    document.getElementById("erreur1").innerHTML= erreur;
     }
+
+    else {
+        if(validNom.exec(Nom.value) == null)
+        
+
+        erreur = "Me prend pas pour un con et saisie et nom qui existe";
+        document.getElementById("erreur1").innerHTML = erreur
+    }
+
+// Prenom
 
     if (Prénom.value == "") {
         event.preventDefault()
         erreur = "Et ton prénom connard";
-        document.getElementById("erreur4").innerHTML= erreur;
+        document.getElementById("erreur2").innerHTML= erreur;
         }
 
+        else if (validPrenom.exec(Prenom.value) ==  null) {
+
+            erreur = "te fous pas de ma gueule met un prenom existant";
+            document.getElementById("erreur2").innerHTML = erreur
+        }
+
+
+
     
- if (code.value.length != 5 || isNaN(code)) {
+ if (Code.value.length != 5 || isNaN(Code.value)) {
     event.preventDefault();
     erreur = "Saisissez un code postal valide à 5 chiffres";
-    document.getElementById("erreur2").innerHTML= erreur;
+    document.getElementById("erreur3").innerHTML= erreur;
     }
 
- if (regex3.exec(ville.value) == null) {
+    else if (validCode.exec(Code.value) ==null) {
+
+        erreur = "nique ta mère a laisser du blanc saisie un code postal";
+        document.getElementById("erreur3").innerHTML = erreur;
+    }
+
+
+
+
+
+
+
+
+ if (regex3.exec(Ville.value) == null) {
     event.preventDefault();
-    erreur = "Veuiller renseigner un nom de ville correct";
-    document.getElementById("erreur5").innerHTML= erreur;
+    erreur = "Passe moi ta ville salope";
+    document.getElementById("erreur4").innerHTML= erreur;
+
+
+
+
+
+
     }
     
  if (regex1.exec(email.value) == null) {
     event.preventDefault();
-    erreur = "Veuiller renseigner un email valide!";
-    document.getElementById("erreur1").innerHTML= erreur
+    erreur = "Veuiller renseigner un email valide! ";
+    document.getElementById("erreur5").innerHTML= erreur
         }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (error) {
+    send.preventDefault();
+    }
