b5e26d4677ad9096c947f8462816273f6c9d88eb
Shadows100 <87180297+Shadows100@users.noreply.github.com>
2021-08-19 14:04:51 +0200

    Bdd en cour

--- BDD/SQL/select join.sql
+++ BDD/SQL/select join.sql
@@ -21,3 +21,20 @@ SELECT hot_nom, hot_ville, hot_categorie
 FROM hotel.hotel
 WHERE hot_ville = 'Bretou' AND hot_categorie > 3;
 
+SELECT hot_nom, hot_categorie, hot_ville, cha_numero
+FROM hotel.hotel
+JOIN chambre 
+ON cha_id = hot_id;
+
+SELECT hot_nom, hot_categorie, hot_ville, cha_numero, cha_capacite
+FROM hotel.hotel
+JOIN chambre
+ON cha_id = hot_id
+WHERE hot_ville = 'Bretou' AND cha_capacite > 1;
+
+SELECT cli_nom, hot_nom, res_date
+FROM hotel.hotel
+JOIN client 
+ON cli_id = hot_id
+JOIN reservation
+ON res_id = cli_id;
